85356d344eb117b1e6ffa939d0f4064e4683a223
Raims Pires <fotsopires10@gmail.com>
2023-10-25 12:13:13 +0100

    ready to push users on line

--- App/settings.py
+++ App/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     
-    # third party middlewares classes
+    # third party middlewares clases
      'user_language_middleware.UserLanguageMiddleware', 
     # custom middlewares classes 
 ]
@@ -129,3 +129,12 @@ STATICFILES_DIR = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone 
 from django.contrib.auth import get_user_model
 from django.contrib.auth import login , logout
 from django.core.mail import send_mail
@@ -11,11 +12,11 @@ from knox.models import AuthToken
 from knox.views import LoginView , LogoutView  
 from knox.auth import TokenAuthentication
 from .serializers import *
-
+from . models import ValidationCodes
 User = get_user_model()
 
 
-class SignUpAPI(generics.GenericAPIView):
+class SignUpUserView(generics.GenericAPIView):
     serializer_class = RegisterUserSerializer
     permission_classes = [AllowAny]
     authentication_classes = (TokenAuthentication,)
@@ -34,7 +35,7 @@ class SignUpAPI(generics.GenericAPIView):
             "users": UserSerializer(user, context=self.get_serializer_context()).data
             })
 
-class SignInAPI(LoginView):
+class SignInUserView(LoginView):
     serializer_class = LoginSerializer
     permission_classes = [AllowAny]
 
@@ -58,13 +59,25 @@ class SignInAPI(LoginView):
         return data
 
 
-class Logout(LogoutView):
+class LogOutView(LogoutView):
     def post(self, request, format=None):
         logout(request=request)
         super().post(request=request, format=format)
         return Response({
             'status':status.HTTP_205_RESET_CONTENT,
             'msg':_("logout successfully")})
+
+
+
+class GetUserView(generics.ListAPIView):
+        
+        serializer_class = UserSerializer
+        
+        
+        def get(self, request, *args, **kwargs):
+            pass
+
+
 
 @api_view(['PUT' , 'PATCH'])
 def update_user_name(request):
@@ -80,19 +93,33 @@ def update_user_name(request):
         raise Exception('Failed to update user name')
 
 
-@api_view(['PATCH'])
-def password_reset(request):
-    data = request.data
-    try:
-        user = User.objects.get(id=data['user_id']) 
-        pass_reset_data = PasswordReset.objects.filter(user = data['user_id'])
-        if  data['new_password'] == data['conf_new_password'] and user.check_password(data['new_password']):
-            user.set_password(data['new_password'])
-        else:
-            return Response({'msg':'password miss match'})
-        return Response(data={'msg':_('password modified successfully')})
-    except:
-        return Response(data={'msg' : _('error while processing the request')})
+
+class ValidationCodesView(generics.CreateAPIView):
+    serializer_class = ValidationCodesSerializer
+    permission_classes = [AllowAny]
+    
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        serialized_data = self.get_serializer(data)
+        serialized_data.is_valid(raise_exception=True)
+        validation_code = serialized_data.save()
+        return Response(data=validation_code.data , status=status.HTTP_201_CREATED)
+
+class PassWordResetView(generics.UpdateAPIView):
+    def put(self, request, *args, **kwargs):
+        try:
+            data = request.data
+            user = User.objects.get(id=data['user_id'])
+            verification_code = ValidationCodes.objects.filter(user = user).order_by('-created_at').first()
+            code_date = verification_code.created_at
+            diff = timezone.now() - code_date
+            if(diff.total_seconds() <= (300)) and data['new_password'] == data['confirm_password'] :
+                user.set_password(data['new_password'])
+            else:
+                return Response({'msg':'password miss match'})
+            return Response(data={'msg':_('password modified successfully')})
+        except:
+            return Response(data={'msg' : _('error while processing the request')})
 
 def send_password_reset_code_mail_to_user(request):
     # here a mail containing the the pass word reset code
