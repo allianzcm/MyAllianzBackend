3757d8b77c09f74d574d97d723beb0e3fae851f9
raimspires20@gmail.com <raimspires20@gmail.com>
2024-03-11 08:46:37 +0100

    setup product

--- services/models.py
+++ services/models.py
@@ -16,16 +16,12 @@ CIVIL_STATUS = [
 ]    
 class Product(AppModel):
     name_en = models.CharField(max_length=255)
-    
     name_fr = models.CharField(max_length=255)
-
     description_en = models.TextField()
-
     description_fr = models.TextField()
-
     image = models.ImageField()
-
-    insurance_type = models.CharField(max_length=20,choices=INSURANCE_TYPE )
+    insurance_type = models.CharField(max_length=20,choices=INSURANCE_TYPE)
+    stars = models.IntegerField()
 
 
 class Question(AppModel):
@@ -40,116 +36,67 @@ class UserAnswer(AppModel):
     
     
 class Subscriber(AppModel):
-
-   
-
     civil_status = models.CharField(max_length=20,choices=CIVIL_STATUS)
-    
     first_name = models.CharField(max_length=255)
-
     last_name = models.CharField(max_length=255)
-
     maiden_name = models.CharField(max_length=255, blank=True, null=True)
-
     dob = models.DateField()
-
     profession = models.CharField(max_length=255)
-
     address = models.TextField()
-
     phone_number = models.CharField(max_length=20)
-
     id_card_number = models.CharField(max_length=50)
-
     email = models.EmailField()
-
     unique_identification_number = models.CharField(max_length=50)
 
 
 
 class Beneficiary(AppModel):
-
     subscriber = models.ForeignKey(Subscriber, on_delete=models.CASCADE)
-
     last_name = models.CharField(max_length=255)
-
     first_name = models.CharField(max_length=255)
-
     age = models.IntegerField()
-
     address = models.TextField()
-
     town_resident = models.CharField(max_length=255)
-
     annuity_manager_phone_number = models.CharField(max_length=20)
-
     annuity_amount = models.DecimalField(max_digits=15, decimal_places=2)
 
 
- 
-
 class  ServiceDuration(AppModel):
-
-    class DurationType(models.TextChoices):
-
-        WEK = 'Weeks'
-
-        MON = 'Months'
-
-        YAE = 'Year'
-
+    DURATION_TYPE = [
+        ("WEK",'Weeks'),
+        ("MON",'Months'),
+        ("YEA",'Year')
+        ]
+    duration_unit =  models.CharField(max_length=5 , choices=DURATION_TYPE)
     length = models.IntegerField()
 
 
-
-
 class ZoneCover(AppModel):
-
     zone_en = models.TextField(max_length=200 )
-
     zone_fr = models.TextField(max_length=200 )
     
 
 
 class AgeRange(AppModel):
-
     above  = models.IntegerField()
-
     below  = models.IntegerField()
 
 
 
 class Pricing(AppModel):
-
     product = models.ForeignKey(to=Product, on_delete=models.RESTRICT)
-
     duration = models.ForeignKey(to=ServiceDuration , on_delete=models.RESTRICT)
-
     total_premium = models.IntegerField()
-
     commission = models.IntegerField()
-
     zone_covert = models.ForeignKey(to=ZoneCover ,on_delete=models.RESTRICT, null=True , blank=True , default=None)
-
     age_range = models.ForeignKey(to=AgeRange ,on_delete=models.RESTRICT, null=True , blank=True , default=None)
 
-
-
-
-# This class named Contrat is a subclass of AppModel.
-
 class Contract(AppModel):
-
-
     approved_by = models.ForeignKey(to=User , on_delete=models.RESTRICT, blank=True , null=True , related_name="contract_approved_by")
-        
     approved_on = models.DateTimeField(blank=True , null=True , default=None)
-    
     commissioned_by = models.ForeignKey(to=User , on_delete=models.RESTRICT , blank=True , null=True , related_name="contract_commissioned")
-
     subscriber_info = models.ForeignKey(to=Subscriber , on_delete=models.RESTRICT)
-
     pricing = models.ForeignKey(to=Pricing , on_delete=models.RESTRICT)
-
     beneficial = models.ForeignKey(to=Beneficiary , on_delete=models.RESTRICT , blank=True , null=True)
-    
+    conceled_at = models.DateField(blank=True, null=True, default=None)
+    conceled_by = models.ForeignKey(to=User , on_delete=models.RESTRICT, blank=True , null=True , related_name="contract_conceled_by")
